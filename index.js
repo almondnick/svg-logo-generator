74dc469a3a8fccd714f1868678c636951b70d6f9
almondnick <nickalmond14@gmail.com>
2023-12-05 15:46:06 -0600

    added functions for writing svg file and prompting user with inquirer, need to make classes for shapes in shapes.js and test

--- index.js
+++ index.js
@@ -1,14 +1,67 @@
-// GIVEN a command-line application that accepts user input **INQUIRER**
-// WHEN I am prompted for text
-// THEN I can enter up to three characters
-// WHEN I am prompted for the text color 
-// THEN I can enter a color keyword (OR a hexadecimal number)
-// WHEN I am prompted for a shape
-// THEN I am presented with a list of shapes to choose from: circle, triangle, and square
-// WHEN I am prompted for the shape's color
-// THEN I can enter a color keyword (OR a hexadecimal number)
-// WHEN I have entered input for all the prompts
-// THEN an SVG file is created named `logo.svg`
-// AND the output text "Generated logo.svg" is printed in the command line
-// WHEN I open the `logo.svg` file in a browser
-// THEN I am shown a 300x200 pixel image that matches the criteria I entered
+const inquirer = require('inquirer');
+const fs = require('fs');
+const { Triangle, Square, Circle } = require('./lib/shapes');
+
+function write(fileName, answers) {
+    let svg = '';
+    svg = '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">';
+    svg += '<g>';
+    svg += `${answers.shape}`;
+
+    let shapeChoice;
+    if (answers.shape === 'Triangle') {
+        shapeChoice = new Triangle();
+        svg += `<polygon points="150, 18 244, 182 56, 182" fill="${answers.shapeColor}"/>`;
+    } else if (answers.shape === 'Square') {
+        shapeChoice = new Square();
+        svg += `<rect x="73" y="40" width="160" height="160" fill="${answers.shapeColor}"/>`;
+    } else {
+        shapeChoice = new Circle();
+        svg += `<circle cx="150" cy="115" r="80" fill="${answers.shapeColor}"/>`;
+    }
+
+    svg += `<text x="150" y="130" text-anchor="middle" font-size="40" fill="${answers.textColor}">${answers.text}</text>`;
+    svg += '</g>';
+    svg += '</svg>';
+
+    fs.writeFile(fileName, svg, (err) => {
+        err ? console.log(err) : console.log('Generated Logo!');
+    });
+}
+
+function promptUser() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: 'What text would you like on your logo? (Enter up to three characters.)',
+                name: 'text',
+            },
+            {
+                type: 'input',
+                message: 'What color would you like your text to be? (Enter a color keyword or a hexadecimal number).',
+                name: 'textColor',
+            },
+            {
+                type: 'list',
+                message: 'What shape would you like your logo to be?',
+                choices: ['Triangle', 'Square', 'Circle'],
+                name: 'shape',
+            },
+            {
+                type: 'input',
+                message: 'What color would you like your shape to be? (Enter a color keyword or a hexadecimal number).',
+                name: 'shapeColor',
+            },
+    ])
+    .then((answers) => {
+        if (answers.text.length > 3) {
+            console.log('Logo text must be 3 characters or less.');
+            promptUser();
+        } else {
+            write('logo.svg', answers);
+        }
+    });
+}
+
+promptUser();
